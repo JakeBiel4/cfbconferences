5eeb3bbdf272925bd2194e577ec0c41773752f96
JakeBiel4 <64647913+JakeBiel4@users.noreply.github.com>
2026-03-26 20:14:47 +0000

    Create functions

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+const conf = document.getElementById("conference");
+const year = document.getElementById("year");
+function fillConf(){
+
+}
+function fillYear(){
+    let val = conf.nodeValue;
+}
+function fill(){
+    
+}
+conf.addEventListener("change", fillYear);
+fillConf();
+fillYear();
