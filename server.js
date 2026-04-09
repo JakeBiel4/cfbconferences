b6d33292e4cea387c8fec7f719c4f405c49dd51a
JakeBiel4 <64647913+JakeBiel4@users.noreply.github.com>
2026-04-09 22:46:15 +0000

    Add Java classes

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 "use strict";
+
+const java = require('java');
+java.classpath.push('com/DataField.class');
+java.classpath.push('com/FieldData.class');
+java.classpath.push('com/TableHeader.class');
+java.classpath.push('com/DataObject.class');
+java.classpath.push('com/DataTable.class');
+java.classpath.push('com/Table.class');
+const DataField = java.import('com.DataField');
+const FieldData = java.import('com.FieldData');
+const TableHeader = java.import('com.TableHeader');
+const DataObject = java.import('com.DataObject');
+const DataTable = java.import('com.DataTable');
+const Table = java.import('com.Table');
+
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
